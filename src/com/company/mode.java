a46f0a3244342f10563b8d731a202c66e5ea29b7
戚高磊 <qigaolei@qq.com>
2018-11-05 19:54:48 +0800

    4444

--- src/com/company/mode.java
+++ src/com/company/mode.java
@@ -3,6 +3,6 @@ package com.company;
 public class mode {
     public static void main(String[] args) {
         System.out.println("kao  都忘了 ");
-        System.out.println("kao  都忘了 ");
+        System.out.println("kao  都忘了333 ");
     }
 }
