cb78b78556be276a136227fb458950f3eecc4c9a
戚高磊 <qigaolei@qq.com>
2018-11-05 19:39:25 +0800

    带的多

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("nihao ");
         System.out.println("haofan");
+        for (int i = 10; i <100 ; i++) {
+            System.out.println(i+"sssss");
+            System.out.println("fen");
+        }
+
     }
 }
---
+++ src/com/company/mode.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class mode {
+    public static void main(String[] args) {
+        System.out.println("kao  都忘了 ");
+    }
+}
